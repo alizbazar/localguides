42755c51f43741112b2667a2dd8ec12540c453a8
peter shodeinde <pshodeinde4@seven.com>
2013-05-25 10:43:34 +0300

    localGuide

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Localguides::Application.config.secret_token = '8ce5f9ed61bc48a738886beb2f313c3a3a4bcd6f94c8e92216936808ae37d2f99a19db4394a38eb7e498d0c29ce1fcbb2d5d3a667702d9a2e1bddae95c98ff2a'
